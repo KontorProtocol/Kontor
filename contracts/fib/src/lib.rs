b0db4b7f9333f32eebe24244f5d2cde2d37723fa
wilfreddenton <wilfred@hey.com>
2025-06-30 15:21:22 -0400

    expanding on sum contract import example

--- contracts/fib/src/lib.rs
+++ contracts/fib/src/lib.rs
@@ -1,18 +1,54 @@
 macros::contract!(name = "fib");
 
+// macros::import!(name = "sum", path = "../sum/wit/contract.wit");
+mod sum {
+    use wasm_wave::wasm::WasmValue;
+
+    use super::foreign;
+
+    const CONTRACT_ID: &str = "sum";
+
+    #[derive(Clone)]
+    pub struct SumArgs {
+        pub x: u64,
+        pub y: u64,
+    }
+
+    impl From<SumArgs> for wasm_wave::value::Value {
+        fn from(value: SumArgs) -> Self {
+            wasm_wave::value::Value::make_record(
+                &wasm_wave::value::Type::record(vec![
+                    ("x".to_string(), wasm_wave::value::Type::U64),
+                    ("y".to_string(), wasm_wave::value::Type::U64),
+                ])
+                .unwrap(),
+                vec![
+                    ("x", wasm_wave::value::Value::make_u64(value.x)),
+                    ("y", wasm_wave::value::Value::make_u64(value.y)),
+                ],
+            )
+            .unwrap()
+        }
+    }
+
+    pub fn sum(args: SumArgs) -> u64 {
+        let expr = format!(
+            "sum({})",
+            wasm_wave::to_string(&wasm_wave::value::Value::from(args)).unwrap(),
+        );
+        let result = foreign::call(CONTRACT_ID, expr.as_str());
+        result.parse::<u64>().unwrap_or(0)
+    }
+}
+
 impl Guest for Fib {
     fn fib(n: u64) -> u64 {
         match n {
             0 | 1 => n,
-            _ => {
-                let expr = format!(
-                    "sum({}, {})",
-                    to_wave(&WaveValue::from(Self::fib(n - 1))).unwrap(),
-                    to_wave(&WaveValue::from(Self::fib(n - 2))).unwrap()
-                );
-                let result = foreign::call("sum", expr.as_str());
-                result.parse::<u64>().unwrap_or(0)
-            }
+            _ => sum::sum(sum::SumArgs {
+                x: Self::fib(n - 1),
+                y: Self::fib(n - 2),
+            }),
         }
     }
 }
--- contracts/sum/src/lib.rs
+++ contracts/sum/src/lib.rs
@@ -1,8 +1,8 @@
 macros::contract!(name = "sum");
 
 impl Guest for Sum {
-    fn sum(x: u64, y: u64) -> u64 {
-        x + y
+    fn sum(args: SumArgs) -> u64 {
+        args.x + args.y
     }
 }
 
