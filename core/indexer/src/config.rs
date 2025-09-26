d83e489e52db80519c9a3a30417b447f9a023873
wilfreddenton <wilfred@hey.com>
2025-09-26 09:59:05 -0400

    fix tests

--- core/indexer/src/config.rs
+++ core/indexer/src/config.rs
@@ -107,11 +107,4 @@ pub struct TestConfig {
         help = "Full path to the taproot key file"
     )]
     pub taproot_key_path: PathBuf,
-
-    #[clap(
-        long,
-        env = "NETWORK",
-        help = "Network to use (bitcoin, testnet, regtest)"
-    )]
-    pub network: bitcoin::Network,
 }
