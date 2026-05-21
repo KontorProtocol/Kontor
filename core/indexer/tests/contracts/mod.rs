69289255342a02774944f6feba51bd47c388415d
Ouziel Slama <ouziel@gmail.com>
2026-05-21 15:07:42 +0200

    Attach/Detach NFT

--- core/indexer/tests/contracts/mod.rs
+++ core/indexer/tests/contracts/mod.rs
@@ -11,6 +11,7 @@ mod crypto_contract;
 mod error_classification;
 mod fib_contract;
 mod file_storage;
+mod native_nft_attach_contract;
 mod native_nft_contract;
 mod native_token_attach_contract;
 mod native_token_contract;
@@ -57,6 +58,7 @@ testlib::regtest_tests! {
     counter_contract,
     crypto_contract,
     fib_contract,
+    native_nft_attach_contract,
     native_nft_contract,
     native_token_attach_contract,
     native_token_contract,
